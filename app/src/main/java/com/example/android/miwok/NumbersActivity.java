bdda8f4149c5fecdf2d1e3542aee7f4a3677f719
Torram <37595361+Torram@users.noreply.github.com>
2018-07-31 20:36:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_numbers);
+
+    }
+}
